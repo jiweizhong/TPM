2c2e14be2f27f469530a83f2ac92f95452629890
root <jwz@jixuanyang.com>
2012-02-27 15:52:12 +0800

    修正连接

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-wget https://github.com/jiweizhong/TPM/master/tpm.php
-wget https://github.com/jiweizhong/TPM/master/tpm.example.conf
+wget https://raw.github.com/jiweizhong/TPM/master/tpm.php
+wget https://raw.github.com/jiweizhong/TPM/master/tpm.example.conf
 rm -r -f /usr/local/bin/tpm
 cp ./tpm.php /usr/local/bin/tpm
 chmod +x /usr/local/bin/tpm
