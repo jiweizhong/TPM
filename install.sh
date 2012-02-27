a1ed743de2e833f04af9dfd09016836ab92785d7
root <jwz@jixuanyang.com>
2012-02-27 15:56:15 +0800

    1

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-wget https://raw.github.com/jiweizhong/TPM/master/tpm.php
-wget https://raw.github.com/jiweizhong/TPM/master/tpm.example.conf
+wget https://raw.github.com/jiweizhong/TPM/master/tpm.php --no-check-certificate
+wget https://raw.github.com/jiweizhong/TPM/master/tpm.example.conf --no-check-certificate
 rm -r -f /usr/local/bin/tpm
 cp ./tpm.php /usr/local/bin/tpm
 chmod +x /usr/local/bin/tpm
